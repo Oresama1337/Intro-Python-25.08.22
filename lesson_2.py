3a5101c68e1a74f7824a3af2be70fafc552a0661
Nelly <nelly.kemppi@gmail.com>
2022-08-29 21:12:03 +0300

    lesson_2 stuff

---
+++ lesson_2.py
@@ -0,0 +1,60 @@
+# print(1 + 2)
+# ctrl + ? english keyboard(-)
+# val_1 = 4  # int
+# val_2 = 0.5  # 5.2 float
+# # result = val_1 * val_2
+# #result = val_1 / val_2
+# #result = val_1 // val_2 #целочисленное деление
+# #result = val_1 % val_2 #остаток от деления
+# result = val_1 ** val_2
+#
+#
+# print(result)
+##################################
+# PEP-8 formatting alt+ctrl+L
+###################################
+# a = 2
+# b = 3
+# c = 4
+# D = b ** 2 - 4 * a * c
+#
+# active_users_count = 1000
+# price = 10
+# total_money = active_users_count * price
+#
+# # PascalCaseObjectNotification
+# # camalCaseObjectNotification
+# # cebab-object-notification
+# RATIO = 100
+# print(total_money, RATIO)
+
+# ############################
+# value_type = type(val_1)
+# value_type = type(val_2)
+# print(val_1, value_type)
+# print(val_2, value_type)
+# # ######################
+# value = 123123123
+# new_value = value
+#
+# print(id(value), id(new_value))
+#
+# value = "123123123"
+# print(value, type(value))
+# result = value + value
+#
+# print(result)
+
+########################### Литералы строк ####################
+
+# my_str1 = "New_string"
+# my_str2 = 'New_string'
+# my_str3 = """New_string"""
+# my_str4 = '''New_string'''
+# value = 1
+# format_str1 = f"New_String"
+# format_str2 = f"My_Value = {value}"
+# format_str3 = f"{value=}"
+# print(format_str1)
+# print(format_str2)
+# print(format_str3)
