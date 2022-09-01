c1af2448b853bfc365ee3e4dd52cee66c656ae71
Nelly <nelly.kemppi@gmail.com>
2022-09-01 20:32:34 +0300

    comment

---
+++ lesson_3.py
@@ -0,0 +1,94 @@
+# bool
+# if
+# input
+
+
+# my_value = None
+# print(my_value, type(my_value))
+
+##################################
+
+# value_1 = 3
+# value_2 = 4
+# result = print(value_1 + value_2)
+# print(result)
+
+##############################
+
+# my_bool_value = False
+# print(my_bool_value, type(my_bool_value))
+
+
+# my_bool_value = 10 == 2
+
+## <, >, <=, >=, ==, !=
+
+## not
+# new_value = not my_bool_value
+# print(my_bool_value, new_value)
+
+# val_1 = True
+# val_2 = False
+
+# result = val_1 and val_2  # logic AND
+
+# print(result)
+
+#########################################
+
+# val_1 = True
+# val_2 = False
+
+# result = val_1 or val_2  # logic OR
+
+# print(result)
+
+########################################
+
+# number = 6
+# colour = "red"
+# result = ((number == 6) or
+#           not (number == 1) and
+#           (colour == "red"))
+# print(result)
+
+#################################
+#
+# value = 123
+#
+# if value > 100:
+#     print(f"Value more than 100")
+# else:
+#     print("Value less or equal than 100")
+
+#if условие
+#   блок если да
+# elif условие 2
+#   блок если да
+#----------------------
+#else:
+#   если нет
+
+
+# value = 0
+#
+# if value > 0:
+#     print(f"Value is positive number")
+# elif value < 0:
+#     print(f"Value is negative number")
+# else:
+#     print("Value is 0")
+
+######################
+#Приведение типов
+#####################
+
+# value_int = 10
+# value_float = float(value_int)
+# print(value_float, type(value_float), value_int)
+#
+# value = 10
+# value = float(value)
+# print(value)
+#
+
