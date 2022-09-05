b07d8cf4d65d21a2bce86106973d5e3a07037e64
Nelly <nelly.kemppi@gmail.com>
2022-09-05 20:59:01 +0300

    методы строк 1.2

--- lesson_4.py
+++ lesson_4.py
@@ -59,12 +59,12 @@ value = 10
 ################### методы строк
 
 
-my_str = "qwerty"
+# my_str = "qwerty"
 # index = 0
-index = -1  #отсчет от конца строки
-
-symbol = my_str[index]  # получение значения по индексу
-print(symbol)
+# index = -1  #отсчет от конца строки
+#
+# symbol = my_str[index]  # получение значения по индексу
+# print(symbol)
 
 # if index < len(my_str):
 #     symbol = my_str[index]  # получение значения по индексу
@@ -72,3 +72,26 @@ print(symbol)
 
 # symbol_count = len(my_str)
 # print(symbol_count)
+
+# my_str[0] = "q" #TypeError: 'str' object does not support item assignment
+
+#   Срезы строк
+# my_str = "qwerty"
+# new_str = my_str[1:5]      #от включено, до - исключено
+# new_str = my_str[1:]      # от и до конца
+# new_str = my_str[:-1]      #от начала и до ...
+# new_str = my_str[:]         # обычная строка
+# print(new_str)
+
+# new_str = my_str[-40:20]
+# print(new_str)
+
+# new_str = "Q" + my_str[1:]
+# new_str = my_str[:3] + "R" + my_str[4:]
+# print(new_str)
+
+# new_str = my_str[::2]   #2 - шаг среза. Значения на четных индексах
+# new_str = my_str[1::2]   #2 - шаг среза. Значения на нечетных индексах
+# new_str = my_str[::-1]  #разворот строки
+# print(new_str)
+
