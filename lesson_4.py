cdf3b785cb93568b0d496c02f328bc546cab05b3
Nelly <nelly.kemppi@gmail.com>
2022-09-05 19:52:36 +0300

    ternary operator

--- lesson_4.py
+++ lesson_4.py
@@ -1,17 +1,32 @@
-# функция input
-# обработка ошибок
-# тернарный оператор
-# цикл с условием
-# методы строк
+# # функция input
+# # обработка ошибок
+# # тернарный оператор
+# # цикл с условием
+# # методы строк
+#
+# value = input("Enter a full number: ")
+# try:
+#     value = int(value)
+#     result = 1 / value
+#     print(value, result)
+#
+# except ValueError:
+#     print("It is not a full number!!!")
+#
+# except ZeroDivisionError:
+#     print("Can not devide on zero!!!")
 
-value = input("Enter a full number: ")
-try:
-    value = int(value)
-    result = 1 / value
-    print(value, result)
 
-except ValueError:
-    print("It is not a full number!!!")
+############################ тернарный оператор
 
-except ZeroDivisionError:
-    print("Can not devide on zero!!!")
+value = -12
+###########
+if value >= 0:
+    result = value
+else:
+    result = value * 2
+print(result)
+##########
+# new_line = positive if result else negative
+result = value if value >= 0 else value * 2
+print(result)
