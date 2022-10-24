4ec21e747a290c7e253e6e1a92cac66d6e646dd3
Nelly <nelly.kemppi@gmail.com>
2022-10-24 20:27:10 +0300

    lesson_16_csv

---
+++ lesson_16_csv.py
@@ -0,0 +1,14 @@
+import csv
+
+def csv_reader(filename: str):
+    data = []
+    with open(filename, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            data.append(row)
+
+    return data
+
+filename = "data.csv"
+data = csv_reader(filename)
+print(data)
