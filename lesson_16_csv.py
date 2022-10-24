2b0930a758a91e3137b9e60ff261babacd637dde
Nelly <nelly.kemppi@gmail.com>
2022-10-24 20:27:51 +0300

    lesson_16_csv_change

--- lesson_16_csv.py
+++ lesson_16_csv.py
@@ -3,7 +3,7 @@ import csv
 def csv_reader(filename: str):
     data = []
     with open(filename, "r") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.reader(csv_file, delimiter=';')
         for row in reader:
             data.append(row)
 
