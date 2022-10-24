37e1d16c6c98b4c142c0de3c2929e4499739d360
Nelly <nelly.kemppi@gmail.com>
2022-10-24 20:53:11 +0300

    lesson_16_csv_change_1

--- lesson_16_csv.py
+++ lesson_16_csv.py
@@ -3,20 +3,29 @@ import csv
 def csv_dict_reader(filename: str):
     data = []
     with open(filename, "r") as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=';')
+        reader = csv.DictReader(csv_file)
         for row in reader:
             data.append(row)
 
     return data
 
-# def csv_writer(filename: str, data: list):
-#     with open(filename, "w") as csv_file:
-#         writer = csv.writer(csv_file)
-#         writer.writerows(data)
+def csv_dict_writer(filename: str, data: list):
+    fieldnames = list(data[0].keys())
+    with open(filename, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
 
 filename = "data.csv"
 data = csv_dict_reader(filename)
 print(data)
+
+for row in data:
+    row['Sum'] = str(int(row['One'] + int(row["Two"])))
+
+filename = "output.csv"
+data = csv_dict_writer(filename, data)
 
 # data.append([100, 200, 300])
 #
