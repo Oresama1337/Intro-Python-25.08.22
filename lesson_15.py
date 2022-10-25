abe941418e168e9da1d366dd71cd004b7cff20c0
Nelly <nelly.kemppi@gmail.com>
2022-10-25 21:11:51 +0300

    lesson_15_"encapsulation"

--- lesson_15.py
+++ lesson_15.py
@@ -5,34 +5,35 @@ import random
 
 class ExampleEncapsulation:
     def __init__(self, dirname: str):
-        self.dirname = dirname
+        self._dirname = dirname
         self._create_directory()
 
 
     def _create_directory(self) -> None:
-        os.makedirs(self.dirname, exist_ok=True)
+        os.makedirs(self._dirname, exist_ok=True)
 
     def create_files(self) -> None:
         for symbol in alphabet:
-            filename = os.path.join(self.dirname, f"{symbol}.txt")
-            self.create_file_from_symbol(filename, symbol)
+            filename = os.path.join(self._dirname, f"{symbol}.txt")
+            self._create_file_from_symbol(filename, symbol)
 
-    def create_file_from_symbol(self, filename: str, symbol: str) -> None:
+    def _create_file_from_symbol(self, filename: str, symbol: str) -> None:
         data = alphabet.replace(symbol, symbol.upper())
         with open(filename, "w") as file:
             file.write(data)
 
     def do_tanos_click(self) -> None:
-        listdir = os.listdir(self.dirname)
+        listdir = os.listdir(self._dirname)
         random.shuffle(listdir)
         for filename in listdir[:len(listdir) // 2]:
-            os.remove(os.path.join(self.dirname, filename))
+            os.remove(os.path.join(self._dirname, filename))
 
 
 dirname = "test1"
 example = ExampleEncapsulation(dirname)
-example.dirname = "test2"
-print(dir(example))
-example._create_directory()
+
+#example._dirname = "test2"
+#print(dir(example))
+#example._create_directory()
 example.create_files()
 example.do_tanos_click()
